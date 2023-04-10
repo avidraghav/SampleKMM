e7b4c4fa42daeb64d2142c9098bf133f2f1c3e55
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-04-10 16:02:03 +0530

    Initial commit

---
+++ shared/src/iosMain/kotlin/com/raghav/samplekmm/Platform.kt
@@ -0,0 +1,9 @@
+package com.raghav.samplekmm
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
