c5a450441accb4ef2bd5b52c92158b47d957623b
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-04-11 12:29:49 +0530

    display data in android UI

--- androidApp/src/main/java/com/raghav/samplekmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/raghav/samplekmm/android/MainActivity.kt
@@ -3,14 +3,33 @@ package com.raghav.samplekmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.raghav.samplekmm.Greeting
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.raghav.samplekmm.SpaceXSDK
+import com.raghav.samplekmm.cache.DatabaseDriverFactory
+import com.raghav.samplekmm.entity.RocketLaunch
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    SampleKmmApp()
                 }
             }
         }
@@ -27,14 +46,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun SampleKmmApp(modifier: Modifier = Modifier) {
+    val sdk = SpaceXSDK(DatabaseDriverFactory(LocalContext.current))
+
+    var launches by remember {
+        mutableStateOf(emptyList<RocketLaunch>())
+    }
+
+    LaunchedEffect(true) {
+        launches = sdk.getLaunches(false)
+    }
+
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(items = launches) {
+            LaunchCard(launch = it)
+        }
+    }
 }
 
-@Preview
 @Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+fun LaunchCard(launch: RocketLaunch, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.fillMaxWidth(), elevation = 8.dp) {
+        Column {
+            Text(text = stringResource(R.string.launch, launch.missionName))
+            Text(text = stringResource(R.string.launch_year, launch.launchYear.toString()))
+            Text(text = stringResource(R.string.launch_details, launch.details.toString()))
+            Text(
+                text = stringResource(
+                    id = R.string.launch_status,
+                    launch.launchSuccess.toString()
+                )
+            )
+        }
     }
 }
