03010e30915ab94d25caffb4499f922e6521e55c
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-04-11 13:31:00 +0530

    android UI changes

--- androidApp/src/main/java/com/raghav/samplekmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/raghav/samplekmm/android/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -21,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -60,7 +62,7 @@ fun SampleKmmApp(modifier: Modifier = Modifier) {
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(8.dp)
+        contentPadding = PaddingValues(16.dp)
     ) {
         items(items = launches) {
             LaunchCard(launch = it)
@@ -71,16 +73,19 @@ fun SampleKmmApp(modifier: Modifier = Modifier) {
 @Composable
 fun LaunchCard(launch: RocketLaunch, modifier: Modifier = Modifier) {
     Card(modifier = modifier.fillMaxWidth(), elevation = 8.dp) {
-        Column {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Text(text = stringResource(R.string.launch, launch.missionName))
+            Text(
+                text = if (launch.launchSuccess == true) stringResource(R.string.success) else stringResource(
+                    R.string.unsuccessful
+                ),
+                color = if (launch.launchSuccess == true) Color.Green else Color.Red
+            )
             Text(text = stringResource(R.string.launch_year, launch.launchYear.toString()))
             Text(text = stringResource(R.string.launch_details, launch.details.toString()))
-            Text(
-                text = stringResource(
-                    id = R.string.launch_status,
-                    launch.launchSuccess.toString()
-                )
-            )
         }
     }
 }
