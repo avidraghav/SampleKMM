4ee0ad757072c93ca42210c4780141c33e95ddeb
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-04-11 13:08:13 +0530

    display data in ios UI

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    let sdk = SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory())
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewModel: .init(sdk: sdk))
+        }
+    }
 }
